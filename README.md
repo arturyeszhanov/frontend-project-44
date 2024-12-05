39dd67a01c77fc407905326a43b210f4e03654c0
Noname <example-hexlet@example.com>
2024-12-06 03:21:45 +0500

    Коммит после 5го пункта

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/arturyeszhanov/frontend-project-44/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/arturyeszhanov/frontend-project-44/actions)
+
+## Demo
+[Watch the gameplay demo on Asciinema](https://asciinema.org/a/sMspFPJQtNd8laLagC7HiRvjM)
---
+++ src/games/even.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => num % 2 === 0;
+
+const playEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const rounds = 3;
+  for (let i = 0; i < rounds; i += 1) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    console.log(`Question: ${randomNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+
+    const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default playEvenGame;
