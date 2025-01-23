6d89070420df31c2c45ce3945c7221f31fe4cb80
Noname <example-hexlet@example.com>
2025-01-23 22:09:31 +0500

    Add even game

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 
 #Requirements
 node.js >= V22.11.0
+
+
+## Демонстрация игры: "Проверка на чётность"
+
+[![asciinema](https://asciinema.org/a/cvwAZzMlVqXb1JRDTnmB2OKHb demo.gif)](https://asciinema.org/connect/87f94db2-d123-4cdc-8064-abd39596e02d)
+
---
+++ src/games/even.js
@@ -0,0 +1,29 @@
+import readlineSync from 'readline-sync';
+
+const isEven = (number) => number % 2 === 0;
+
+const playEvenGame = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+  const rounds = 3;
+  for (let i = 0; i < rounds; i += 1) {
+    const number = Math.floor(Math.random() * 100);
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ').toLowerCase();
+
+    const correctAnswer = isEven(number) ? 'yes' : 'no';
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+    console.log('Correct!');
+  }
+
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default playEvenGame;
