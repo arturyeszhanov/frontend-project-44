f59924301754cc048da5fe0d3fd5e02a6d26fee3
Noname <example-hexlet@example.com>
2025-01-26 02:19:15 +0500

    Работа над ошибками

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
-import { getRandomNumber } from '../utils.js';
+import getRandomNumber from '../utils.js';
 
 const isEven = (number) => number % 2 === 0;
 
