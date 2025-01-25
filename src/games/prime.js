aec1867a97e49dc889961c203b295e47b48520ba
Noname <example-hexlet@example.com>
2025-01-26 00:33:52 +0500

    Устранение проблем с линтингом

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,7 +2,7 @@ import runGame from '../index.js';
 
 const isPrime = (num) => {
   if (num <= 1) return false;
-  for (let i = 2; i <= Math.sqrt(num); i++) {
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) return false;
   }
   return true;
