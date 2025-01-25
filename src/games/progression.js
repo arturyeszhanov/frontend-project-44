d1012beb5dbc6bece47d97fe10fd0bf0970c0ee6
Noname <example-hexlet@example.com>
2025-01-26 01:20:38 +0500

    Удаление комментарий из файла src/games/progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,19 +3,19 @@ import runGame from '../index.js';
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 const generateProgression = () => {
-  const length = getRandomInt(5, 10); // Длина прогрессии (не менее 5 чисел)
-  const start = getRandomInt(1, 20); // Начальное число прогрессии
-  const step = getRandomInt(2, 10); // Шаг прогрессии
+  const length = getRandomInt(5, 10);
+  const start = getRandomInt(1, 20);
+  const step = getRandomInt(2, 10);
   const progression = [];
 
   for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step);
   }
 
-  const hiddenIndex = getRandomInt(0, length - 1); // Индекс, в котором будет спрятано число
+  const hiddenIndex = getRandomInt(0, length - 1);
   const correctAnswer = progression[hiddenIndex];
 
-  progression[hiddenIndex] = '..'; // Заменяем одно из чисел на '..'
+  progression[hiddenIndex] = '..';
 
   const question = progression.join(' ');
 
