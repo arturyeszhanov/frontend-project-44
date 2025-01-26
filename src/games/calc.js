22fc3b1001c80d4e28af7746ee1a8ebf54223d7f
Noname <example-hexlet@example.com>
2025-01-26 07:59:39 +0500

    Исправление ошибок в файле calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,38 +1,32 @@
 import runGame from '../index.js';
 
 const operations = ['+', '-', '*'];
-
+const rule = 'What is the result of the expression?';
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const calculateExpression = (number1, number2, operation) => {
+  switch (operation) {
+    case '+':
+      return number1 + number2;
+    case '-':
+      return number1 - number2;
+    case '*':
+      return number1 * number2;
+    default:
+      throw new Error(`Unknown operation: $operation`);
+  }
+};
 
 const generateRound = () => {
   const number1 = getRandomInt(1, 20);
   const number2 = getRandomInt(1, 20);
   const operation = operations[getRandomInt(0, operations.length - 1)];
 
-  let question;
-  let correctAnswer;
+  let question = `${number1} ${operation} ${number2}`;;
+  let correctAnswer = calculateExpression(number1, number2, operation);
 
-  switch (operation) {
-    case '+':
-      question = `${number1} + ${number2}`;
-      correctAnswer = number1 + number2;
-      break;
-    case '-':
-      question = `${number1} - ${number2}`;
-      correctAnswer = number1 - number2;
-      break;
-    case '*':
-      question = `${number1} * ${number2}`;
-      correctAnswer = number1 * number2;
-      break;
-    default:
-      throw new Error('Unknown operation: $operation');
-  }
-
-  return [question, correctAnswer];
+  return [question, String(correctAnswer)];
 };
-
-const rule = 'What is the result of the expression?';
 
 const runCalcGame = () => {
   runGame(rule, generateRound);
