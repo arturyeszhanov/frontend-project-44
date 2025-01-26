c49421bc2706264edfc203a26ced12e994102654
Noname <example-hexlet@example.com>
2025-01-26 08:13:46 +0500

    Детерминированные функции: progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,31 +1,32 @@
 import runGame from '../index.js';
 
+const rule = 'What number is missing in the progression?';
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const generateProgression = () => {
-  const length = getRandomInt(5, 10);
-  const start = getRandomInt(1, 20);
-  const step = getRandomInt(2, 10);
+const generateProgression = (start, step, length, hiddenIndex) => {
   const progression = [];
-
   for (let i = 0; i < length; i += 1) {
     progression.push(start + i * step);
   }
 
-  const hiddenIndex = getRandomInt(0, length - 1);
   const correctAnswer = progression[hiddenIndex];
-
   progression[hiddenIndex] = '..';
-
   const question = progression.join(' ');
 
-  return [question, correctAnswer];
+  return [question, String(correctAnswer)];
 };
 
-const rule = 'What number is missing in the progression?';
+const generateRound = () => {
+  const length = getRandomInt(5, 10);
+  const start = getRandomInt(1, 20);
+  const step = getRandomInt(2, 10);
+  const hiddenIndex = getRandomInt(0, length - 1);
+
+  return generateProgression(start, step, length, hiddenIndex);
+};
 
 const runProgressionGame = () => {
-  runGame(rule, generateProgression);
+  runGame(rule, generateRound);
 };
 
 export default runProgressionGame;
