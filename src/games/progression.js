2e649d2cd92012997c313d72f2e712bfec984ede
Noname <example-hexlet@example.com>
2025-01-28 00:02:26 +0500

    Работа над файлом progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,12 +3,7 @@ import getRandomNumber from '../utils.js';
 
 const rule = 'What number is missing in the progression?';
 
-const generateProgression = (start, step, length, hiddenIndex) => {
-  const progression = [];
-  for (let i = 0; i < length; i += 1) {
-    progression.push(start + i * step);
-  }
-
+const generateProgression = (progression, hiddenIndex) => {
   const correctAnswer = progression[hiddenIndex];
   progression[hiddenIndex] = '..';
   const question = progression.join(' ');
@@ -20,9 +15,15 @@ const generateRound = () => {
   const length = getRandomNumber(5, 10);
   const start = getRandomNumber(1, 20);
   const step = getRandomNumber(2, 10);
-  const hiddenIndex = getRandomNumber(0, length - 1);
 
-  return generateProgression(start, step, length, hiddenIndex);
+  const progression = [];
+  for (let i = 0; i < length; i++) {
+    progression.push(start + i * step);
+  }
+
+  const hiddenIndex = getRandomNumber(0, progression.length - 1);
+
+  return generateProgression(progression, hiddenIndex);
 };
 
 const runProgressionGame = () => {
