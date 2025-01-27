5ac7cf2acd36ffacb7763855c6a90a12e75dd67a
Noname <example-hexlet@example.com>
2025-01-27 23:05:06 +0500

    Исправление ошибок #3

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import readlineSync from 'readline-sync';
 
+const rounds = 3;
+
 const runGame = (rule, generateRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rule);
 
-  const rounds = 3;
   for (let i = 0; i < rounds; i += 1) {
     const [question, correctAnswer] = generateRound();
     console.log(`Question: ${question}`);
